68c836444f362d3e6ad79de9e60649a0fa636e08
yashu <yashavanth07a@gmail.com>
2026-06-26 14:14:12 +0530

    Merge

--- day1/EDA_project.py
+++ day1/EDA_project.py
@@ -25,3 +25,51 @@ print(df.isnull().sum())
 median_age = df['Age'].median()
 df['Age'] = df['Age'].fillna(median_age)
 print(median_age)
+
+median_spending=df['Spending'].median()
+df['Spending']=df['Spending'].fillna(median_age)
+print(median_spending)
+
+mean_age=df['Age'].mean()
+df['Age']=df['Age'].fillna(mean_age)
+print(mean_age)
+
+mean_spending=df['Spending'].mean()
+df['Spending']=df['Spending'].fillna(mean_age)
+print(mean_spending)
+
+
+plt.figure(figsize=(7,4))
+df['Spending'].hist(bins=10,color='cyan',edgecolor='black')
+plt.title("Distribution of Spending")
+plt.xlabel('Spending Amount')
+plt.ylabel('Number of Customers')
+plt.show()
+
+correlation = df.corr(numeric_only=True)
+print(correlation)
+
+print("plotting Correlation Heatmap")
+plt.figure(figsize=(7,4))
+sns.heatmap(correlation,annot=True,cmap='coolwarm',fmt='.2f')
+plt.title('Correlation Matrix')
+plt.show()
+
+plt.figure(figsize=(7,4))
+plt.scatter(df['Age'],df['Spending'],color='red',alpha=0.5)
+plt.title('Age vs Spending')
+plt.xlabel('Age')
+plt.ylabel('Spending')
+plt.show()
+
+plt.figure(figsize=(7,4))
+sns.boxplot(x=df['Age'],color='lightgreen')
+plt.title("Boxplot of Customer Age")
+plt.xlabel('Age')
+plt.show()
+
+
+print("Find the Outliers in age")
+outliers=(df['Age']>100)
+print("Found Outliers(s):")
+print(outliers)
