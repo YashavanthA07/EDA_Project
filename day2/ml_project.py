0be53b72f77fe9e2c0feba21580e3c819beeca22
yashu <yashavanth07a@gmail.com>
2026-06-29 10:40:11 +0530

    feature

--- day2/ml_project.py
+++ day2/ml_project.py
@@ -1,3 +1,4 @@
+from pandas.core.interchange import dataframe
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
@@ -38,6 +39,35 @@ def main():
 
     df["LogRuns"] = np.log1p(df["R"])
     print(f"Log Transformation Applied. New skewness: {df['LogRuns'].skew():.2f} (closer to 0 is perfectly balanced)\n")
- 
+    print(df.head())
+    df['Team_ID'] = ['Team'+str(np.random.randint(1,150)) for _ in range(len(df))]
+
+    if TargetEncoder is not None:
+        print("Applying Target Encoder")
+
+        encoder = TargetEncoder()
+        print("Missing values in W:", df['W'].isnull().sum())
+        df = df.dropna(subset=['W'])
+        df['Team_ID_Encoded'] = encoder.fit_transform(df['Team_ID'], df['W'])
+
+        print(f"\nTarget Encoding Example (First 5 Rows):")
+        print(df[['Team_ID', 'Team_ID_Encoded', 'W']].head())
+
+    
+    else:
+        print("Category Encoders not installed")
+
+
+
+    features_to_test=['R','HR','BB','BA']
+
+    X_features = df[features_to_test].fillna(0)
+    Y_traget = df['W']
+
+    selector = SelectKBest(score_func = mutual_info_regression,k=2)
+        
+    winning_features = selector.get_support()
+    best_feature = X_features.columns[winning_features].tolist()
+    print(f"Best Features:{best_feature}")
 if __name__ == "__main__":
     main()
